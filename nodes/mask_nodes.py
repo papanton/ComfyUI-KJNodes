a4e24844387c6480eccea5e5492c0de68205685f
Antonis Papantoniou <papanton@usc.edu>
2024-08-30 19:05:38 -0500

    Update mask_nodes.py

--- nodes/mask_nodes.py
+++ nodes/mask_nodes.py
@@ -1273,52 +1273,65 @@ class GradientMap:
 Apply gradient map to mask
 """
 
-    def apply_gradient_map(self, image, start_color, mid_color, end_color, mid_point, opacity, layer_mask=None):
-        def create_gradient_array(start_color, mid_color, end_color, mid_point):
-            def hex_to_rgb(hex_color):
+def apply_gradient_map(self, image, start_color, mid_color, end_color, mid_point, opacity, layer_mask=None):
+    def create_gradient_array(start_color, mid_color, end_color, mid_point):
+        def hex_to_rgb(hex_color):
+            try:
                 return tuple(int(hex_color.lstrip('#')[i:i+2], 16) for i in (0, 2, 4))
+            except ValueError:
+                raise ValueError(f"Invalid color format: {hex_color}. Expected format: '#RRGGBB'")
 
-            start_rgb = hex_to_rgb(start_color)
-            mid_rgb = hex_to_rgb(mid_color)
-            end_rgb = hex_to_rgb(end_color)
-            
-            mid_index = int(255 * mid_point)
-            gradient1 = np.array([np.linspace(start_rgb[i], mid_rgb[i], mid_index + 1) for i in range(3)]).T
-            gradient2 = np.array([np.linspace(mid_rgb[i], end_rgb[i], 256 - mid_index) for i in range(3)]).T
-            return np.vstack((gradient1[:-1], gradient2))
+        start_rgb = hex_to_rgb(start_color)
+        mid_rgb = hex_to_rgb(mid_color)
+        end_rgb = hex_to_rgb(end_color)
+        
+        mid_index = int(255 * mid_point)
+        gradient1 = np.array([np.linspace(start_rgb[i], mid_rgb[i], mid_index + 1) for i in range(3)]).T
+        gradient2 = np.array([np.linspace(mid_rgb[i], end_rgb[i], 256 - mid_index) for i in range(3)]).T
+        return np.vstack((gradient1[:-1], gradient2))
 
-        gradient_array = create_gradient_array(start_color, mid_color, end_color, mid_point)
+    gradient_array = create_gradient_array(start_color, mid_color, end_color, mid_point)
 
-        gradient_image = Image.fromarray(np.uint8(gradient_array.reshape(1, -1, 3).repeat(50, axis=0)))
-        gradient_tensor = pil2tensor(gradient_image)
-        ret_images = []
-        for img in image:
-            pil_image = tensor2pil(img)
-            
-            # Convert to grayscale to get luminance
-            gray_image = np.array(pil_image.convert('L'))
-            
-            # Apply gradient map
-            gradient_mapped = gradient_array[gray_image]
-            
-            # Preserve luminance of original image
-            original_array = np.array(pil_image)
-            luminance = np.sum(original_array * [0.299, 0.587, 0.114], axis=2, keepdims=True) / 255.0
-            gradient_mapped = gradient_mapped * luminance + original_array * (1 - luminance)
-            
-            gradient_mapped_image = Image.fromarray(np.uint8(gradient_mapped))
-            
-            # Apply opacity
-            if opacity < 100:
-                gradient_mapped_image = Image.blend(pil_image, gradient_mapped_image, opacity / 100)
-            
-            # Apply mask if provided
-            if layer_mask is not None:
-                mask = tensor2pil(layer_mask).convert('L')
-                pil_image.paste(gradient_mapped_image, (0, 0), mask)
-            else:
-                pil_image = gradient_mapped_image
-            
-            ret_images.append(pil2tensor(pil_image))
+    gradient_image = Image.fromarray(np.uint8(gradient_array.reshape(1, -1, 3).repeat(50, axis=0)))
+    gradient_tensor = pil2tensor(gradient_image)
 
-        return (torch.cat(ret_images, dim=0), gradient_tensor)
+    # Ensure we're working with a single image
+    if image.dim() == 4 and image.size(0) == 1:
+        image = image.squeeze(0)
+    elif image.dim() != 3:
+        raise ValueError("Input image should be a single 3D tensor (C, H, W)")
+
+    pil_image = tensor2pil(image)
+    
+    # Convert to grayscale to get luminance
+    gray_image = np.array(pil_image.convert('L'))
+    
+    # Apply gradient map
+    gradient_mapped = gradient_array[gray_image]
+    
+    # Preserve luminance of original image
+    original_array = np.array(pil_image)
+    luminance = np.sum(original_array * [0.299, 0.587, 0.114], axis=2, keepdims=True) / 255.0
+    gradient_mapped = gradient_mapped * luminance + original_array * (1 - luminance)
+    
+    gradient_mapped_image = Image.fromarray(np.uint8(gradient_mapped))
+    
+    # Apply opacity
+    if opacity < 100:
+        gradient_mapped_image = Image.blend(pil_image, gradient_mapped_image, opacity / 100)
+    
+    # Apply mask if provided
+    if layer_mask is not None:
+        if layer_mask.dim() == 3 and layer_mask.size(0) == 1:
+            layer_mask = layer_mask.squeeze(0)
+        elif layer_mask.dim() != 2:
+            raise ValueError("Layer mask should be a single 2D tensor (H, W)")
+        
+        mask = tensor2pil(layer_mask).convert('L')
+        pil_image.paste(gradient_mapped_image, (0, 0), mask)
+    else:
+        pil_image = gradient_mapped_image
+    
+    result_tensor = pil2tensor(pil_image)
+
+    return (result_tensor, gradient_tensor)
